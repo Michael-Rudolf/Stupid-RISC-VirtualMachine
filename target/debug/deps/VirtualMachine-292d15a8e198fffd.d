3192322db8c6730f093656da8067488b1d8ff50f
MichaelRudol <98760082+MichaelRudol@users.noreply.github.com>
2025-02-16 20:19:31 +0100

    Made some text green and added an installation tutorial.

---
+++ target/debug/deps/VirtualMachine-292d15a8e198fffd.d
@@ -0,0 +1,9 @@
+/Users/michaelrudolf/Development/Rust/VirtualMachine/target/debug/deps/VirtualMachine-292d15a8e198fffd: src/main.rs src/instruction.rs src/machine/mod.rs src/machine/machine.rs src/machine/machine_execution.rs
+
+/Users/michaelrudolf/Development/Rust/VirtualMachine/target/debug/deps/VirtualMachine-292d15a8e198fffd.d: src/main.rs src/instruction.rs src/machine/mod.rs src/machine/machine.rs src/machine/machine_execution.rs
+
+src/main.rs:
+src/instruction.rs:
+src/machine/mod.rs:
+src/machine/machine.rs:
+src/machine/machine_execution.rs:
